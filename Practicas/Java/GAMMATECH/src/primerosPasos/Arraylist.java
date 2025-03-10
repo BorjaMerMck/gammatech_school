5aa2f4264c1832fd9215cb763f57f1042d2e6884
BorjaMerMck <borjacursos146@gmail.com>
2025-03-10 06:11:38 +0100

    Subida de los ejercicios de Java

---
+++ Practicas/Java/GAMMATECH/src/primerosPasos/Arraylist.java
@@ -0,0 +1,23 @@
+package primerosPasos;
+import java.util.ArrayList;
+
+public class Arraylist {
+
+
+	    public static void main(String[] args) {
+
+	    	ArrayList<String> lista = new ArrayList<>();
+
+	        lista.add("Manzana");
+	        lista.add("platano");
+	        lista.add("Pera");
+
+	        System.out.println("Lista completa: " + lista);
+
+
+	        System.out.println("\nElementos de la lista:");
+	        for (String elemento : lista) {
+	            System.out.println(elemento);
+	        }
+	    }
+	}
